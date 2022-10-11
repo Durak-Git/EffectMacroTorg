ae93cb82d09dffeb15666257f883ff481bfc5f35
Durak-Git <97435195+Durak-Git@users.noreply.github.com>
2022-10-11 14:00:08 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,37 @@
-export function torgBonus(rollTotal) {
-    let bonus;
+export function torgBonu(rollTotal) {
+    let bonu;
     if (rollTotal == 1) {
-        bonus = -10
+        bonu = -10
     } else if (rollTotal == 2) {
-        bonus = -8
+        bonu = -8
     } else if (rollTotal <= 4) {
-        bonus = -6
+        bonu = -6
     } else if (rollTotal <= 6) {
-        bonus = -4
+        bonu = -4
     } else if (rollTotal <= 8) {
-        bonus = -2
+        bonu = -2
     } else if (rollTotal <= 10) {
-        bonus = -1
+        bonu = -1
     } else if (rollTotal <= 12) {
-        bonus = 0
+        bonu = 0
     } else if (rollTotal <= 14) {
-        bonus = 1
+        bonu = 1
     } else if (rollTotal == 15) {
-        bonus = 2
+        bonu = 2
     } else if (rollTotal == 16) {
-        bonus = 3
+        bonu = 3
     } else if (rollTotal == 17) {
-        bonus = 4
+        bonu = 4
     } else if (rollTotal == 18) {
-        bonus = 5
+        bonu = 5
     } else if (rollTotal == 19) {
-        bonus = 6
+        bonu = 6
     } else if (rollTotal == 20) {
-        bonus = 7
+        bonu = 7
     } else if (rollTotal >= 21) {
-        bonus = 7 + Math.ceil((rollTotal - 20) / 5)
+        bonu = 7 + Math.ceil((rollTotal - 20) / 5)
     }
-    return bonus
+    return bonu
 
 }
+
